b3a0e8062b43639474384ab8753a6912f5cb86d0
“MARIA <MDFORTINO@GMAIL.COM>
2019-01-18 16:22:44 -0500

    added middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 // app.get("/", (request, response) => {
 //   response.send("Hello World")
 // })
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set("view engine", "hbs")
 
@@ -12,8 +14,19 @@ app.get("/:name", function (req, res) {
   res.render('index',{ name: req.params.name})
 })
 
+app.post("/", (req, res) => {
+    res.send(`hello ${req.body.name}`)
+  })
+  
+app.post("/", (req, res) => {
+    res.render("index", {
+      name: req.body.firstName,
+    })
+  })
 
-
+app.get("/", (req, res) => {
+    res.render("welcome")
+  }) 
 
 app.listen(4000, () => {
   console.log("app listening on port 4000")
