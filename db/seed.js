1262e8f6b9ff7c748f9c6d5bf3555724e8227f28
“MARIA <MDFORTINO@GMAIL.COM>
2019-01-22 11:29:59 -0500

    created user seed

--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,10 @@ const { Question } = require("../models/Question");
 
 User.find({}).remove(() => {
   Question.find({}).remove(() => {
+    let bugs = User.create({
+        email: "bugsbunny@gmail.com",
+        password: createPassword("bugsbunny")
+      })
   });
 });
 
